b877db2da961736394e64eddbdbccc17340c257f
Remon Lawendi <remon.lawendi@gmail.com>
2023-03-25 21:32:42 +0200

    Done The project

---
+++ Day 2 - Random Hex Color Generator/script.js
@@ -0,0 +1,14 @@
+window.addEventListener('load', () => {
+  const color = document.querySelector('.generated-color')
+  const btn_generate = document.querySelector('.generate')
+
+  const colorGenerator = () => {
+    const randomColor = Math.random().toString(16).substring(2, 8)
+    document.body.style.backgroundColor = `#${randomColor}`
+    color.innerHTML = ` #${randomColor} `
+  }
+
+  btn_generate.addEventListener('click', () => {
+    colorGenerator()
+  })
+})
